8538887d65b87cb35b6417ac2944b56049d52d7b
Vinicius Morais Araújo <vinidev.morais@gmail.com>
2026-09-08 16:19:33 -0300

    feat(worker): wire message handler and scoped execution in Worker background service

--- src/PromoBot.Worker/Worker.cs
+++ src/PromoBot.Worker/Worker.cs
@@ -1,16 +1,48 @@
+using PromoBot.Application.Interfaces;
+using PromoBot.Application.UseCases;
+
 namespace PromoBot.Worker;
 
-public class Worker(ILogger<Worker> logger) : BackgroundService
+public class Worker(ILogger<Worker> logger, ITelegramGateway telegramGateway, IServiceScopeFactory scopeFactory) : BackgroundService
 {
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-    {
-        while (!stoppingToken.IsCancellationRequested)
-        {
-            if (logger.IsEnabled(LogLevel.Information))
-            {
-                logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            }
-            await Task.Delay(1000, stoppingToken);
-        }
-    }
+   private readonly ILogger<Worker> _logger = logger;
+   private readonly ITelegramGateway _telegramGateway = telegramGateway;
+   private readonly IServiceScopeFactory _scopeFactory = scopeFactory;
+   protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+   {
+      _logger.LogInformation("PromoBot Worker run...");
+
+      _telegramGateway.OnMessageReceived += HandleMessageReceivedAsync;
+      
+      await _telegramGateway.StartAsync(stoppingToken);
+      
+      _logger.LogInformation("PromoBot Worker started");
+
+      try
+      {
+         await Task.Delay(Timeout.Infinite, stoppingToken);
+      }
+      catch (Exception e)
+      {
+         Console.WriteLine(e);
+         throw;
+      }
+   }
+   
+   private async Task HandleMessageReceivedAsync(long chatId, int messageId, string text)
+   {
+      _logger.LogInformation("Mensagem recebida do Chat {ChatId} (Id: {MessageId})", chatId, messageId);
+      
+      try
+      {
+         using var scope = _scopeFactory.CreateScope();
+         var useCase = scope.ServiceProvider.GetRequiredService<ProcessIncomingMessageUseCase>();
+
+         await useCase.ExecuteAsync(chatId, messageId, text);
+      }
+      catch (Exception ex)
+      {
+         _logger.LogInformation(ex, "Erro ao processar mensagem {MessageId} do Chat {ChatId}", messageId, chatId);
+      }
+   }
 }
