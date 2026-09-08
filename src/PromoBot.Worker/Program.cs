89ff1f0ccec4d3a6bbc5ca404f00e6d62b1ddaf7
Vinicius Morais Araújo <vinidev.morais@gmail.com>
2026-09-08 16:19:31 -0300

    feat(worker): configure dependency injection and services in Program.cs

--- src/PromoBot.Worker/Program.cs
+++ src/PromoBot.Worker/Program.cs
@@ -1,7 +1,31 @@
+using Microsoft.EntityFrameworkCore;
+using PromoBot.Application.Interfaces;
+using PromoBot.Application.UseCases;
+using PromoBot.Domain.Entities;
+using PromoBot.Infrastructure.Persistence;
+using PromoBot.Infrastructure.Telegram;
 using PromoBot.Worker;
 
 var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.Configure<TelegramSettings>(
+    builder.Configuration.GetSection(TelegramSettings.SectionName));
+
+builder.Services.Configure<List<FilterRule>>(
+    builder.Configuration.GetSection("FilterRules"));
+
+builder.Services.AddDbContext<PromoBotDataContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IPromotionRepository, PromotionRepository>();
+builder.Services.AddScoped<INotifier, SavedMessagesNotifier>();
+
+builder.Services.AddSingleton<ITelegramGateway, TelegramGateway>();
+
+builder.Services.AddScoped<ProcessIncomingMessageUseCase>();
+
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
+
 host.Run();
