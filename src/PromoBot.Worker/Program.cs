db1e94efd5ed63eed58053b16aaca9d9e9ee4cf5
Vinicius Morais Araújo <vinidev.morais@gmail.com>
2026-09-08 20:21:32 -0300

    feat(Program): update DbContext to use pooling and implement logging for message formatting

--- src/PromoBot.Worker/Program.cs
+++ src/PromoBot.Worker/Program.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using Microsoft.EntityFrameworkCore;
 using PromoBot.Application.Interfaces;
 using PromoBot.Application.UseCases;
@@ -14,11 +15,26 @@ builder.Services.Configure<TelegramSettings>(
 builder.Services.Configure<List<FilterRule>>(
     builder.Configuration.GetSection("FilterRules"));
 
-builder.Services.AddDbContext<PromoBotDataContext>(options =>
+builder.Services.AddDbContextPool<PromoBotDataContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+WTelegram.Helpers.Log = (level, message) =>
+{
+    var localMessage = Regex.Replace(message, @"\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}Z", match =>
+    {
+        if (DateTime.TryParse(match.Value, out var utcDate))
+        {
+            return utcDate.ToLocalTime().ToString("yyyy-MM-dd HH:mm:ss");
+        }
+        return match.Value;
+    });
+
+    Console.WriteLine(localMessage);
+};
+
 builder.Services.AddScoped<IPromotionRepository, PromotionRepository>();
-builder.Services.AddScoped<INotifier, SavedMessagesNotifier>();
+
+builder.Services.AddScoped<INotifier, TelegramBotNotifier>();
 
 builder.Services.AddSingleton<ITelegramGateway, TelegramGateway>();
 
