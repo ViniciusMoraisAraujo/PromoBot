a468f4519e1bfbb336f4e52e278d9a4c7ecb34aa
Vinicius Morais Araújo <vinidev.morais@gmail.com>
2026-09-09 12:15:35 -0300

    feat(ITelegramGateway): add Messages channel for incoming message handling

--- src/PromoBot.Application/Interfaces/ITelegramGateway.cs
+++ src/PromoBot.Application/Interfaces/ITelegramGateway.cs
@@ -1,8 +1,12 @@
+using System.Threading.Channels;
+using PromoBot.Domain.Models;
+
 namespace PromoBot.Application.Interfaces;
 
 public interface ITelegramGateway
 {
     Task StartAsync(CancellationToken ct = default);
     Task SendToSavedMessagesAsync(string message, CancellationToken ct = default);
-    event Func<long, int, string, Task>? OnMessageReceived;
+    
+    ChannelReader<IncomingMessage> Messages { get; }
 }
